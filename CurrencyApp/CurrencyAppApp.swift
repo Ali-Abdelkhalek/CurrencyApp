f361367c56e30a057fc7e252f5d4c1ce23cf2093
ali.abdelkhalek@si-ware.com <aliabdelkhalek96@gmail.com>
2023-12-09 10:53:15 +0200

    Initial Commit

---
+++ CurrencyApp/CurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyAppApp.swift
+//  CurrencyApp
+//
+//  Created by Ali Abdelkhalek on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
